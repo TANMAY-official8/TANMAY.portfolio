37bb1db07cb8c9ccac474907c5a91eda3470ea19
Tanmay <tanmaysinghmar8@gmail.com>
2026-07-23 21:11:23 +0530

    Removed unused file

--- script.js
+++
@@ -1,9 +0,0 @@
- const nav = document.getElementById("nav");
-
- window.addEventListener("scroll", () => {
-     if (window.scrollY > 0) {
-         nav.classList.add("scrolled");
-     } else {
-         nav.classList.remove("scrolled");
-  }
- });
